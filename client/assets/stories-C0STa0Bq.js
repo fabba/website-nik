bd97f29557882490577441ac63743bae9b66bd15
FabianVoorter <FabianVoorter@gmail.com>
2026-02-18 20:04:41 +0100

    Updates

---
+++ client/assets/stories-C0STa0Bq.js
@@ -0,0 +1,1 @@
+import{w as o,p as t}from"./chunk-WWGJGFF6-DaTaPSdi.js";import{C as r}from"./navigation-container-BE-MtQ21.js";const n=o(function(){return t.jsx(r,{children:"Stories"})});export{n as default};
---
+++ server/index.js
@@ -0,0 +1,246 @@
+import { jsx, jsxs } from "react/jsx-runtime";
+import { PassThrough } from "node:stream";
+import { createReadableStreamFromReadable } from "@react-router/node";
+import { ServerRouter, UNSAFE_withComponentProps, Outlet, UNSAFE_withErrorBoundaryProps, isRouteErrorResponse, Meta, Links, ScrollRestoration, Scripts } from "react-router";
+import { isbot } from "isbot";
+import { renderToPipeableStream } from "react-dom/server";
+const streamTimeout = 5e3;
+function handleRequest(request, responseStatusCode, responseHeaders, routerContext, loadContext) {
+  if (request.method.toUpperCase() === "HEAD") {
+    return new Response(null, {
+      status: responseStatusCode,
+      headers: responseHeaders
+    });
+  }
+  return new Promise((resolve, reject) => {
+    let shellRendered = false;
+    let userAgent = request.headers.get("user-agent");
+    let readyOption = userAgent && isbot(userAgent) || routerContext.isSpaMode ? "onAllReady" : "onShellReady";
+    let timeoutId = setTimeout(
+      () => abort(),
+      streamTimeout + 1e3
+    );
+    const { pipe, abort } = renderToPipeableStream(
+      /* @__PURE__ */ jsx(ServerRouter, { context: routerContext, url: request.url }),
+      {
+        [readyOption]() {
+          shellRendered = true;
+          const body = new PassThrough({
+            final(callback) {
+              clearTimeout(timeoutId);
+              timeoutId = void 0;
+              callback();
+            }
+          });
+          const stream = createReadableStreamFromReadable(body);
+          responseHeaders.set("Content-Type", "text/html");
+          pipe(body);
+          resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: responseStatusCode
+            })
+          );
+        },
+        onShellError(error) {
+          reject(error);
+        },
+        onError(error) {
+          responseStatusCode = 500;
+          if (shellRendered) {
+            console.error(error);
+          }
+        }
+      }
+    );
+  });
+}
+const entryServer = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  default: handleRequest,
+  streamTimeout
+}, Symbol.toStringTag, { value: "Module" }));
+const links = () => [{
+  rel: "preconnect",
+  href: "https://fonts.googleapis.com"
+}, {
+  rel: "preconnect",
+  href: "https://fonts.gstatic.com",
+  crossOrigin: "anonymous"
+}, {
+  rel: "stylesheet",
+  href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+}];
+function Layout({
+  children
+}) {
+  return /* @__PURE__ */ jsxs("html", {
+    lang: "en",
+    children: [/* @__PURE__ */ jsxs("head", {
+      children: [/* @__PURE__ */ jsx("meta", {
+        charSet: "utf-8"
+      }), /* @__PURE__ */ jsx("meta", {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      }), /* @__PURE__ */ jsx(Meta, {}), /* @__PURE__ */ jsx(Links, {})]
+    }), /* @__PURE__ */ jsxs("body", {
+      children: [children, /* @__PURE__ */ jsx(ScrollRestoration, {}), /* @__PURE__ */ jsx(Scripts, {})]
+    })]
+  });
+}
+const root = UNSAFE_withComponentProps(function App() {
+  return /* @__PURE__ */ jsx(Outlet, {});
+});
+const ErrorBoundary = UNSAFE_withErrorBoundaryProps(function ErrorBoundary2({
+  error
+}) {
+  let message = "Oops!";
+  let details = "An unexpected error occurred.";
+  let stack;
+  if (isRouteErrorResponse(error)) {
+    message = error.status === 404 ? "404" : "Error";
+    details = error.status === 404 ? "The requested page could not be found." : error.statusText || details;
+  }
+  return /* @__PURE__ */ jsxs("main", {
+    className: "pt-16 p-4 container mx-auto",
+    children: [/* @__PURE__ */ jsx("h1", {
+      children: message
+    }), /* @__PURE__ */ jsx("p", {
+      children: details
+    }), stack]
+  });
+});
+const route0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  ErrorBoundary,
+  Layout,
+  default: root,
+  links
+}, Symbol.toStringTag, { value: "Module" }));
+function Container({ children }) {
+  return /* @__PURE__ */ jsxs("div", { className: " h-dvh flex-row basis-full flex", children: [
+    /* @__PURE__ */ jsxs("div", { className: "flex flex-10 flex-col bg-emerald-50 items-center", children: [
+      /* @__PURE__ */ jsx("a", { href: "/", className: "w-full block text-center", children: "Nik" }),
+      /* @__PURE__ */ jsx("a", { href: "/logo", className: "p-4 ml-2 mr-2 border-b border-gray-30", children: "Logo's" }),
+      /* @__PURE__ */ jsx("a", { href: "/stories", className: "p-4 ml-2 mr-2 border-b border-gray-30", children: "Stories" }),
+      /* @__PURE__ */ jsx("a", { href: "/contact", className: "p-4 ml-2 mr-2 border-b border-gray-30", children: "Contact" })
+    ] }),
+    /* @__PURE__ */ jsx(
+      "div",
+      {
+        className: `flex-90 flex-col page bg-emerald-100`,
+        children
+      }
+    )
+  ] });
+}
+function meta({}) {
+  return [{
+    title: "New React Router App"
+  }, {
+    name: "description",
+    content: "Welcome to React Router!"
+  }];
+}
+const home = UNSAFE_withComponentProps(function Home() {
+  return /* @__PURE__ */ jsx(Container, {
+    children: "test"
+  });
+});
+const route1 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  default: home,
+  meta
+}, Symbol.toStringTag, { value: "Module" }));
+const logo = UNSAFE_withComponentProps(function Logo() {
+  return /* @__PURE__ */ jsx(Container, {
+    children: "Logo"
+  });
+});
+const route2 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  default: logo
+}, Symbol.toStringTag, { value: "Module" }));
+const stories = UNSAFE_withComponentProps(function Stories() {
+  return /* @__PURE__ */ jsx(Container, {
+    children: "Stories"
+  });
+});
+const route3 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  default: stories
+}, Symbol.toStringTag, { value: "Module" }));
+const contact = UNSAFE_withComponentProps(function Contact() {
+  return /* @__PURE__ */ jsx(Container, {
+    children: "Email: nikheemskerk@gmail.com"
+  });
+});
+const route4 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  default: contact
+}, Symbol.toStringTag, { value: "Module" }));
+const serverManifest = { "entry": { "module": "/assets/entry.client-wMV1gpTa.js", "imports": ["/assets/chunk-WWGJGFF6-DaTaPSdi.js"], "css": [] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": true, "module": "/assets/root-BPrxFHpA.js", "imports": ["/assets/chunk-WWGJGFF6-DaTaPSdi.js"], "css": ["/assets/root-C-1d___K.css"], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 }, "routes/home": { "id": "routes/home", "parentId": "root", "path": void 0, "index": true, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": false, "module": "/assets/home-C_PycSES.js", "imports": ["/assets/chunk-WWGJGFF6-DaTaPSdi.js", "/assets/navigation-container-BE-MtQ21.js"], "css": [], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 }, "routes/logo": { "id": "routes/logo", "parentId": "root", "path": "logo", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": false, "module": "/assets/logo-C3eM5cYi.js", "imports": ["/assets/chunk-WWGJGFF6-DaTaPSdi.js", "/assets/navigation-container-BE-MtQ21.js"], "css": [], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 }, "routes/stories": { "id": "routes/stories", "parentId": "root", "path": "stories", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": false, "module": "/assets/stories-C0STa0Bq.js", "imports": ["/assets/chunk-WWGJGFF6-DaTaPSdi.js", "/assets/navigation-container-BE-MtQ21.js"], "css": [], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 }, "routes/contact": { "id": "routes/contact", "parentId": "root", "path": "contact", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": false, "module": "/assets/contact--sXmr1_g.js", "imports": ["/assets/chunk-WWGJGFF6-DaTaPSdi.js", "/assets/navigation-container-BE-MtQ21.js"], "css": [], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 } }, "url": "/assets/manifest-302364fe.js", "version": "302364fe", "sri": void 0 };
+const assetsBuildDirectory = "build\\client";
+const basename = "/";
+const future = { "unstable_optimizeDeps": false, "unstable_subResourceIntegrity": false, "v8_middleware": false, "v8_splitRouteModules": false, "v8_viteEnvironmentApi": false };
+const ssr = true;
+const isSpaMode = false;
+const prerender = [];
+const routeDiscovery = { "mode": "lazy", "manifestPath": "/__manifest" };
+const publicPath = "/";
+const entry = { module: entryServer };
+const routes = {
+  "root": {
+    id: "root",
+    parentId: void 0,
+    path: "",
+    index: void 0,
+    caseSensitive: void 0,
+    module: route0
+  },
+  "routes/home": {
+    id: "routes/home",
+    parentId: "root",
+    path: void 0,
+    index: true,
+    caseSensitive: void 0,
+    module: route1
+  },
+  "routes/logo": {
+    id: "routes/logo",
+    parentId: "root",
+    path: "logo",
+    index: void 0,
+    caseSensitive: void 0,
+    module: route2
+  },
+  "routes/stories": {
+    id: "routes/stories",
+    parentId: "root",
+    path: "stories",
+    index: void 0,
+    caseSensitive: void 0,
+    module: route3
+  },
+  "routes/contact": {
+    id: "routes/contact",
+    parentId: "root",
+    path: "contact",
+    index: void 0,
+    caseSensitive: void 0,
+    module: route4
+  }
+};
+export {
+  serverManifest as assets,
+  assetsBuildDirectory,
+  basename,
+  entry,
+  future,
+  isSpaMode,
+  prerender,
+  publicPath,
+  routeDiscovery,
+  routes,
+  ssr
+};
